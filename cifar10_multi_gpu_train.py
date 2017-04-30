796640f7b111daac50253e59d704ff3087951f48
gy <marcusgao@163.com>
2017-04-30 12:35:05 -0400

    change max step

--- cifar10_multi_gpu_train.py
+++ cifar10_multi_gpu_train.py
@@ -54,9 +54,9 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', '../train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000,
+tf.app.flags.DEFINE_integer('max_steps', 100000,
                             """Number of batches to run.""")
-tf.app.flags.DEFINE_integer('num_gpus', 8,
+tf.app.flags.DEFINE_integer('num_gpus', 1,
                             """How many GPUs to use.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
